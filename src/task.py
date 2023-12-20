3113754acc3248ac0f4ea0c647a70ef057ce2b21
Jean_Loup <jean-loup.gonzalez@univ-tlse3.fr>
2023-12-20 15:43:32 +0100

    corrected typo + added __eq__ method

--- src/task.py
+++ src/task.py
@@ -7,15 +7,27 @@ import numpy as np
 
 
 class Task:
-    def __init__(self, indentifier: int, size: int):
-        self.indentifier = indentifier
+    def __init__(self, identifier: int, size: int, a=None, b=None, x=None, time=None):
+        self.identifier = identifier
         self.size = size
-        self.a = np.random.rand(size, size)
-        self.b = np.random.rand(
-            size,
-        )
-        self.x = np.zeros((size,))
-        self.time = 0.0
+        if a is None:
+            self.a = np.random.rand(size, size)
+        else:
+            self.a = a
+        if b is None:
+            self.b = np.random.rand(
+                size,
+            )
+        else:
+            self.b = b
+        if x is None:
+            self.x = np.zeros((size,))
+        else:
+            self.x = x
+        if time is None:
+            self.time = 0.0
+        else:
+            self.time = time
 
     def work(self) -> float:
         time_start = perf_counter()
@@ -27,7 +39,7 @@ class Task:
     def to_json(self) -> str:
         class_info = {
             "size": self.size,
-            "id": self.indentifier,
+            "id": self.identifier,
             "a": self.a.tolist(),
             "b": self.b.tolist(),
             "x": self.x.tolist(),
@@ -38,7 +50,32 @@ class Task:
     @classmethod
     def from_json(cls, task_info_json: str) -> "Task":
         task_info = json.loads(task_info_json)
-        return Task(task_info.get("id"), task_info.get("size"))
+        return Task(
+            task_info.get("id"),
+            task_info.get("size"),
+            task_info.get("a"),
+            task_info.get("b"),
+            task_info.get("x"),
+            task_info.get("time"),
+        )
+
+    def __eq__(self, other_task: "Task") -> bool:
+        if not isinstance(other_task, Task):
+            return False
+        elif self.identifier != other_task.identifier:
+            return False
+        elif self.size != other_task.size:
+            return False
+        elif not np.array_equal(self.a, other_task.a):
+            print(self.a)
+            print(other_task.a)
+            return False
+        elif not np.array_equal(self.b, other_task.b):
+            return False
+        elif not np.array_equal(self.x, other_task.x):
+            return False
+        else:
+            return True
 
 
 if __name__ == "__main__":
@@ -47,5 +84,6 @@ if __name__ == "__main__":
     # print(ma_tache.work())
     json_tache = ma_tache.to_json()
     deuxieme_tache = Task.from_json(json_tache)
-    print(deuxieme_tache.work())
-    print(ma_tache.work())
+    # print(deuxieme_tache.work())
+    # print(ma_tache.work())
+    print(ma_tache == deuxieme_tache)
